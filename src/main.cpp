afa4cc68e29fc328edc3a0d6265ac66d2d2d119c
jclc <johannes.lakkisto@helsinki.fi>
2017-02-16 05:27:06 +0200

    added sqlite3

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,94 @@
+#include "version.h"
 #include <iostream>
 #include <unistd.h>
 #include <csignal>
 #include <uWS.h>
+#include "optparse/optparse.h"
+#include "database/database.h"
 
-int listenPort = 3000;
+// Function forward declarations
+void signalHandler(int signal);
+void printHelp();
+void printVersion();
+
+int main(int argc, char *argv[]) {
+	// Initialise options
+	int listenPort = 80;
+
+	Database db;
+
+	// Parse CLI arguments
+	struct optparse options;
+	optparse_init(&options, argv);
+	struct optparse_long longopts[] = {
+		{"help", 'h', OPTPARSE_NONE},
+		{"version", 'V', OPTPARSE_NONE},
+		{"port", 'p', OPTPARSE_REQUIRED},
+		{0}
+	};
+	for (int option = 0; (option = optparse_long(&options, longopts, NULL)) != -1; ) {
+		switch (option) {
+		case 'h':
+			printHelp();
+			exit(0);
+		case 'V':
+			printVersion();
+			exit(0);
+		case 'p':
+			listenPort = atoi(options.optarg);
+			if (listenPort <= 0) {
+				std::cerr << "Error: Invalid port " << listenPort << std::endl;
+				exit(1);
+			}
+			continue;
+		case '?':
+			std::cerr << options.errmsg << std::endl;
+			exit(1);
+		}
+	}
+
+	signal(SIGINT, signalHandler);
+	uWS::Hub hub;
+	hub.onMessage([](uWS::WebSocket<uWS::SERVER> ws, char *message, size_t length, uWS::OpCode opCode) {
+		std::cout << "Received message:" << std::endl << message;
+		std::cout << "Length: " << length << ", OpCode: " << opCode << std::endl;
+		ws.send(message, length, opCode);
+	});
+
+	bool success;
+
+	success = hub.listen(listenPort);
+
+	if (!success) {
+		std::cerr << "Error: Port rejected" << std::endl;
+		exit(2);
+	}
+
+	std::cout << "Starting server on port " << listenPort << std::endl;
+	hub.run();
+	return 0;
+}
 
 // Signal handler function for gracefully terminating the program
 void signalHandler(int signal) {
 	if (signal == SIGINT) {
 		std::cout << std::endl << "Interrupt signal received, terminating" << std::endl;
-		exit(signal);
+		exit(0);
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	signal(SIGINT, signalHandler);
-	uWS::Hub hub;
-	hub.onMessage([](uWS::WebSocket<uWS::SERVER> ws, char *message, size_t length, uWS::OpCode opCode) {
-			ws.send(message, length, opCode);
-	});
+void printHelp() {
+	std::cout
+	<< "webchat-server [options]" << std::endl
+	<< "  Options:" << std::endl
+	<< "  -h | --help" << std::endl
+	<< "    Display this help message" << std::endl
+	<< "  -V | --version" << std::endl
+	<< "    Display program version" << std::endl
+	<< "  -p | --port <port number>" << std::endl
+	<< "    Which listen port to use" << std::endl;
+}
 
-	hub.listen(listenPort);
-	std::cout << "Starting server"
-	hub.run();
-	return 0;
+void printVersion() {
+	std::cout << PROGRAM_VERSION << std::endl;
 }
---
+++ src/version.h
@@ -0,0 +1,8 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// Edit program version in this file
+
+#define PROGRAM_VERSION "alpha"
+
+#endif //VERSION_H
